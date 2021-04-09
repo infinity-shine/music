92d99629a24d90281cf9bbe8a5678dc307a73a32
Jack <1908365891@qq.com>
2021-04-09 13:46:51 +0800

    第三次提交

--- src/com/exmaple/helloscala.java
+++ src/com/exmaple/helloscala.java
@@ -1,14 +1,8 @@
 package com.exmaple;
 
-public class hello
-{
-
+public class helloscala {
 
     public static void main(String[] args) {
-        System.out.println("hello java");
-
-        System.out.println("hello spark");
-
         System.out.println("hello scala");
     }
 }
